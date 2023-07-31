c8f3832d564a81cc41bcf669090996085bab9847
kuuote <znmxodq1@gmail.com>
2023-08-01 00:12:13 +0900

    save

--- hoge.ts
+++ hoge.ts
@@ -1,4 +1,28 @@
-import { isString } from "https://deno.land/x/unknownutil@v3.2.0/mod.ts";
+import * as u from "https://deno.land/x/unknownutil@v3.2.0/mod.ts";
 
-const a = Deno.readTextFileSync(Deno.args[0]);
-console.log(a);
+type Event = {
+  action: string;
+  issue: {
+    body: string;
+    number: number;
+  };
+};
+
+const isEvent: u.Predicate<Event> = u.isObjectOf({
+  action: u.isString,
+  issue: u.isObjectOf({
+    body: u.isString,
+    number: u.isNumber,
+  }),
+});
+
+isEvent(42);
+
+async function main() {
+  const a = JSON.parse(await Deno.readTextFile(Deno.args[0])) as unknown;
+  u.assert(a, isEvent);
+  console.log(a);
+  return 0;
+}
+
+Deno.exit(await main());
